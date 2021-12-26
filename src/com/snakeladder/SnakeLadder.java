8ada49cf4de9a961796b42f9a2fd35fd99329739
atul080 <atulsrivastav16k26@gmaial.com>
2021-12-26 21:00:14 +0530

    Added:  Rollig dice functionality added.

--- src/com/snakeladder/SnakeLadder.java
+++ src/com/snakeladder/SnakeLadder.java
@@ -1,12 +1,22 @@
 package com.snakeladder;
 
+import java.util.*;
 public class SnakeLadder {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int currentPosition=0;
-		System.out.println("Game started with current position as 0.");
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int currentPosition=0;
+        System.out.println("Game started with current position as 0.");
 
-	}
+        //creating object of Random
+        Random rm= new Random();
+
+        //rolling a dice to get number between 1 to 6
+        int diceValue=rm.nextInt(6);
+
+        //printing the dice value
+        System.out.print("You got: "+diceValue);
+
+    }
 
 }
