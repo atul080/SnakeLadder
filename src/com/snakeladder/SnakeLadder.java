c62a48f2794f710b513db5e186d55102806d02ba
atul080 <atulsrivastav16k26@gmaial.com>
2021-12-26 20:52:07 +0530

    Added: Start of Snake Ladder added.

---
+++ src/com/snakeladder/SnakeLadder.java
@@ -0,0 +1,12 @@
+package com.snakeladder;
+
+public class SnakeLadderStart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int currentPosition=0;
+		System.out.println("Game started with current position as 0.");
+
+	}
+
+}
