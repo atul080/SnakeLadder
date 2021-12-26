a9ef4bb54d0f8b8c7e7543707cbc6f9815bfc50d
atul080 <atulsrivastav16k26@gmaial.com>
2021-12-26 21:13:47 +0530

    Added:  Double player functionality added.

--- src/com/snakeladder/SnakeLadder.java
+++ src/com/snakeladder/SnakeLadder.java
@@ -3,55 +3,91 @@ package com.snakeladder;
 import java.util.*;
 public class SnakeLadder {
 
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        int currentPosition=0;
-        int minValOfDice=1;
-        int maxValOfDice=6;
-        int winPosition=100;
-        int diceRollCount=0;
-        System.out.println("*******Game started with current position as 0.*******");
+
+    static int minValOfDice=1;
+    static int maxValOfDice=6;
+    static int winPosition=100;
+    static int playerOneDiceRollCount=0;
+    static int playerTwoDiceRollCount=0;
+
+    public static int diceRoll(int currentPosition,int player)
+    {
+        System.out.println("**************** Player "+player+" turn.****************");
+
+        if(player==1)
+            playerOneDiceRollCount++;
+        else
+            playerTwoDiceRollCount++;
 
         //creating object of Random
         Random rm= new Random();
 
+        //rolling a dice to get number between 1 to 6
+        int diceValue= rm.nextInt((maxValOfDice - minValOfDice) + minValOfDice)+1;
 
-        //looping to get win position
-        while(currentPosition<winPosition)
+        //getting option for steps
+        int step = rm.nextInt(3);
+
+        if (step == 0) {
+              System.out.println("You got no play.");
+              return currentPosition;
+        } else if (step == 1)
         {
-            //rolling a dice to get number between 1 to 6
-            int diceValue= rm.nextInt((maxValOfDice - minValOfDice) + minValOfDice)+1;
+               System.out.println("You got ladder. So moving by " + diceValue + " steps.");
 
-            //printing the dice value
-            System.out.println("------------------Rolling dice.You got: "+diceValue);
+            //checking if new position is exact 100 or more than that.
+            int newPosition = currentPosition + diceValue;
 
-            int step = rm.nextInt(3);
-
-            if (step == 0) {
-                System.out.println("You got no play.");
-                System.out.println("Your new postion is: " + currentPosition);
-            } else if (step == 1) {
-                System.out.println("You got ladder. So moving by " + diceValue + " steps.");
-
-                //checking if new position is exact 100 or more than that.
-                int newPosition = currentPosition + diceValue;
-                if(newPosition<=winPosition)
-                    currentPosition=newPosition;
-                else
-                    System.out.println("Sorry! You need exact "+(winPosition-currentPosition)+" to win.");
-
-                System.out.println("Your new postion is: " + currentPosition);
-            } else {
-                System.out.println("Opps!! You got a snake bite. Moving down by " + diceValue + " position.");
-                currentPosition = currentPosition - diceValue;
-                if (currentPosition < 0)
-                    currentPosition = 0;
-                System.out.println("Your new postion is: " + currentPosition);
+            if(newPosition<winPosition)
+            {
+                currentPosition = newPosition;
+                System.out.println("Current:"+currentPosition);
+                currentPosition=diceRoll(currentPosition,player);
             }
-            diceRollCount++;
+            else if (newPosition==winPosition)
+            {
+                currentPosition=newPosition;
+                System.out.println("Congratulations!!! Player "+player+" won the game in total "+playerOneDiceRollCount+" plays.");
+                System.exit(0);
+            }
+            else
+                System.out.println("Sorry! You need exact "+(winPosition-currentPosition)+" to win.");
+
+            return currentPosition;
+        } else {
+            System.out.println("Opps!! You got a snake bite. Moving down by " + diceValue + " position.");
+            currentPosition = currentPosition - diceValue;
+            if (currentPosition < 0)
+                currentPosition = 0;
         }
 
-        System.out.println("Congratulations!!! You won the game in total "+diceRollCount+" plays.");
+        return currentPosition;
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        System.out.println("*******Game started with Player 1 current position as 0.*******");
+
+        int PlyOneCurrentPosition=0;
+        int PlyTwoCurrentPosition=0;
+
+
+        //loop for calling method for dice roll
+        while(true)
+        {
+
+            PlyOneCurrentPosition=SnakeLadderDoublePlayer.diceRoll(PlyOneCurrentPosition,1);
+            System.out.println("Player 1 new postion is: " + PlyOneCurrentPosition);
+            PlyTwoCurrentPosition=SnakeLadderDoublePlayer.diceRoll(PlyTwoCurrentPosition,2);
+            System.out.println("Player 2 new postion is: " + PlyTwoCurrentPosition);
+        }
+
+
     }
 
 }
