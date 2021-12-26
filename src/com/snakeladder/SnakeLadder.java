89350e8da9214d43d1cba88b83075faccafed899
atul080 <atulsrivastav16k26@gmaial.com>
2021-12-26 21:03:20 +0530

    Added:  Step options functionality added.

--- src/com/snakeladder/SnakeLadder.java
+++ src/com/snakeladder/SnakeLadder.java
@@ -6,16 +6,39 @@ public class SnakeLadder {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         int currentPosition=0;
+        int minValOfDice=1;
+        int maxValOfDice=6;
         System.out.println("Game started with current position as 0.");
 
         //creating object of Random
         Random rm= new Random();
 
         //rolling a dice to get number between 1 to 6
-        int diceValue=rm.nextInt(6);
+        int diceValue= rm.nextInt((maxValOfDice - minValOfDice) + minValOfDice)+1;
 
         //printing the dice value
-        System.out.print("You got: "+diceValue);
+        System.out.println("You got: "+diceValue);
+
+        //getting options for steps
+        int step=rm.nextInt(3);
+
+        if(step==0)
+        {
+            System.out.println("You got no play.");
+        }
+        else if(step==1)
+        {
+            System.out.println("You got ladder. So moving by "+diceValue+" steps.");
+            currentPosition=currentPosition+diceValue;
+        }
+        else
+        {
+            System.out.println("Opps!! You got a snake bite. Moving down by "+diceValue+" position.");
+            currentPosition=currentPosition-diceValue;
+            if(currentPosition<0)
+                currentPosition=0;
+        }
+
 
     }
 
